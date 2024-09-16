3d9ea403002222f0f3056fc3a9c18a1eb0c6f4fd
Giovani <gdrosdalima@gmail.com>
2024-09-15 23:31:55 -0300

    Updates

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@
     name: 'Chrome Built-In AI',
     description: 'social net that provides the best discounts and coupons from partners daily, automatically.',
     coverUrl: '/showcases/chrome-ai.webm',
-    visitedCount: localStorage.getItem('Chrome Built-In AI') || 0,
+    // visitedCount: localStorage.getItem('Chrome Built-In AI') || 0,
     tags: ['Dev', 'AI', 'Chrome', 'React'],
     link: 'https://dlgiovani.github.io/chrome-ai-webapp/',
     year: "2024"
@@ -12,7 +12,7 @@
     name: 'Gamefied Portfolio',
     description: 'my old portfolio.',
     coverUrl: '/showcases/game-portfolio.webm',
-    visitedCount: localStorage.getItem('Gamefied Portfolio') || 0,
+    // visitedCount: localStorage.getItem('Gamefied Portfolio') || 0,
     tags: ['portfolio', 'React'],
     link: 'https://dlgiovani.github.io/gameport/',
     year: "2024"
@@ -21,7 +21,7 @@
     name: 'Economiza Brasil',
     description: 'social net that provides the best discounts and coupons from partners daily, automatically.',
     coverUrl: 'mtpp/economizabrasil.webp',
-    visitedCount: localStorage.getItem('Economiza Brasil') || 0,
+    // visitedCount: localStorage.getItem('Economiza Brasil') || 0,
     tags: ['Bot', 'Automation', 'API'],
     link: 'https://economizabrasil.com/?utm_source=portfolio',
     year: "2023"
@@ -30,7 +30,7 @@
     name: 'Sage 50',
     description: 'a beautiful custom start page for your browser',
     coverUrl: 'https://lh3.googleusercontent.com/Ubrdar1zVOBSoUOLHxWnHN5kRD34QAJeRN6gu2hE51Mb96gLSm60Pf_cKIsmEtKmy0gYtL1WG0mAIbIrriD3BkLNEg=s1280-w1280-h800',
-    visitedCount: localStorage.getItem('Sage 50') || 0,
+    // visitedCount: localStorage.getItem('Sage 50') || 0,
     tags: ['Chrome API', 'Extension'],
     link: 'https://chromewebstore.google.com/detail/sage50/mfkcidgkcjacmjddjpfecppgdenjjccm?hl=pt-BR&authuser=0',
     year: "2023"
@@ -39,7 +39,7 @@
     name: 'PH newtab',
     description: 'a productivity homepage for Product Hunt users',
     coverUrl: 'https://lh3.googleusercontent.com/cUz5AIMJHlDvxhWcWh-ospTFn7hgFlu8ESNGHcXKf6JGwAwZNlUnBy6qFyNm8Yw9ZWL06UhcZfj8nfDy3QQbgjhWqw=s800-w800-h500',
-    visitedCount: localStorage.getItem('PH newtab') || 0,
+    // visitedCount: localStorage.getItem('PH newtab') || 0,
     tags: ['Chrome API', 'Extension', 'React', 'DaisyUI'],
     link: 'https://www.producthunt.com/products/ph-newtab',
     year: "2024"
@@ -48,7 +48,7 @@
     name: 'Business Card',
     description: 'a personal card made in React',
     coverUrl: '/mtpp/business-card.webp',
-    visitedCount: localStorage.getItem('Business Card') || 0,
+    // visitedCount: localStorage.getItem('Business Card') || 0,
     tags: ['React', 'Frontend'],
     link: 'https://dlgiovani.github.io/digital-business-card/',
     year: "2024"
@@ -57,7 +57,7 @@
     name: 'Catalogue',
     description: 'a marketplace for your business',
     coverUrl: 'mtpp/my_simple_marketplace_landpage.webp',
-    visitedCount: localStorage.getItem('Catalogue') || 0,
+    // visitedCount: localStorage.getItem('Catalogue') || 0,
     tags: ['Fullstack', 'CDN', 'Product'],
     link: 'https://www.youtube.com/watch?v=P4ej2iySmug',
     year: "2023"
@@ -66,7 +66,7 @@
     name: 'Notes App',
     description: 'a markdown supported notes app',
     coverUrl: 'mtpp/notes-app.webp',
-    visitedCount: localStorage.getItem('Notes App') || 0,
+    // visitedCount: localStorage.getItem('Notes App') || 0,
     tags: ['React', 'Markdown'],
     link: 'https://dlgiovani.github.io/notes-app/',
     year: "2024"
@@ -75,7 +75,7 @@
     name: 'Terrain Wanderer',
     description: 'a graphical application to navigate on a random generated terrain',
     coverUrl: 'mtpp/terrain-wanderer.webp',
-    visitedCount: localStorage.getItem('Terrain Wanderer') || 0,
+    // visitedCount: localStorage.getItem('Terrain Wanderer') || 0,
     tags: ['Graphics'],
     link: 'https://openprocessing.org/sketch/2188030',
     year: "2023"
@@ -84,7 +84,7 @@
     name: 'Tesla Vortex',
     description: 'a graphical application to generate Tesla\'s vortexes',
     coverUrl: 'mtpp/tesla-vortex.webp',
-    visitedCount: localStorage.getItem('Tesla Vortex') || 0,
+    // visitedCount: localStorage.getItem('Tesla Vortex') || 0,
     tags: ['Graphic', 'Math'],
     link: 'https://github.com/dlgiovani/Tesla-Vortex',
     year: "2022"
@@ -93,7 +93,7 @@
     name: 'Nimbus',
     description: 'a cozy place to relax and feel the weather 🌥️',
     coverUrl: 'mtpp/nimbus.webp',
-    visitedCount: localStorage.getItem('Nimbus') || 0,
+    // visitedCount: localStorage.getItem('Nimbus') || 0,
     tags: ['Frontend', 'API', 'Embed'],
     link: '/nimbus/nimbus.html',
     year: "2023"
@@ -102,7 +102,7 @@
     name: 'Dragon',
     description: 'just visit it. Trust me.',
     coverUrl: '',
-    visitedCount: localStorage.getItem('Dragon') || 0,
+    // visitedCount: localStorage.getItem('Dragon') || 0,
     tags: ['???'],
     link: '/dragon/dragon.html',
     year: "2023"
@@ -111,7 +111,7 @@
     name: 'Cattos',
     description: 'a very cute website 🐾',
     coverUrl: 'mtpp/cattos.webp',
-    visitedCount: localStorage.getItem('Cattos') || 0,
+    // visitedCount: localStorage.getItem('Cattos') || 0,
     tags: ['Frontend', 'API', 'Cute!'],
     link: '/cattos/index.html',
     year: "2023"
@@ -120,7 +120,7 @@
     name: 'What we gaming\'',
     description: 'a website to give you info and help you decide a new game to play',
     coverUrl: 'mtpp/what-we-gamin.webp',
-    visitedCount: localStorage.getItem('What we gaming\'') || 0,
+    // visitedCount: localStorage.getItem('What we gaming\'') || 0,
     tags: ['Fullstack', 'API', 'Django'],
     link: 'https://github.com/dlgiovani/what-we-gamin',
     year: "2023"
@@ -129,7 +129,7 @@
     name: 'Beacon',
     description: 'a billboard democratizer',
     coverUrl: 'mtpp/beacon_landpage.webp',
-    visitedCount: localStorage.getItem('Beacon') || 0,
+    // visitedCount: localStorage.getItem('Beacon') || 0,
     tags: ['Fullstack', 'Product', 'Django'],
     link: '',
     year: "?"
@@ -138,7 +138,7 @@
     name: 'Tamashi',
     description: '(Top Secret)',
     coverUrl: '',
-    visitedCount: localStorage.getItem('Tamashi') || 0,
+    // visitedCount: localStorage.getItem('Tamashi') || 0,
     tags: ['???'],
     link: '',
     year: "?"
@@ -147,7 +147,7 @@
     name: 'Visihouse',
     description: 'A 5D map viewer!',
     coverUrl: '',
-    visitedCount: localStorage.getItem('Visihouse') || 0,
+    // visitedCount: localStorage.getItem('Visihouse') || 0,
     tags: ['5D map', 'Virtual Tour', 'React'],
     link: '',
     year: "2025"
